99e4b2cfeb2222d50e87fb263175f83ee9782676
Pete-Mp256 <pete.mbithi@strathmore.edu>
2024-09-27 07:51:05 +0300

    Second commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is my first page</h1>
+    <h1>This is my first page</h1>
+    <?php
+    require_once "load.php";
+    print $obj -> user_age("Alex",2004);
+    ?>
+</body>
+</html>
